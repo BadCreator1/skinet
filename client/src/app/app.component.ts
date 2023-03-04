6f836cc6f327446d6f3b86bf03fd9e03179611e5
BadCreator1 <badcreator391@gmail.com>
2023-03-04 12:44:12 +0200

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,12 +15,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.http.get<IPagination>('https://localhost:5001/api/products?pageSize=50')
-      .subscribe(
-        (response) => {
-          this.products = response.data;
-        }, error => {
-          console.log(error);
-        })
+   
   }
 }
